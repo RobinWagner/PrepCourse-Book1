695914601439fa4a5f0cb623293b7fe441704cf7
Robin Wagner <robin-wagner@web.de>
2016-12-08 20:14:00 +0000

    Exercise 7 - Chapter 1 completed

---
+++ Chapter1_TheBasics/Exercise7.rb
@@ -0,0 +1,4 @@
+# Exercise 7
+
+# Syntax error occurred: Closing parenthesis (')') was used instead of closing curly braces ('}')
+# An opening curly brace was not closed. (May have occurred when creating hash.)

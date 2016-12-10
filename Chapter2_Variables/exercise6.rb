6547c89ec509ad989041cae7edf5fc189bfce827
Robin Wagner <robin-wagner@web.de>
2016-12-10 00:32:40 +0000

    Chapter 2 Exercise 6 - updated

--- Chapter2_Variables/exercise6.rb
+++ Chapter2_Variables/exercise6.rb
@@ -1,3 +1,3 @@
 # Exercise 6
 
-# Local variable 'shoes' is referenced which is undefined in the scope in which it is called
+# Local variable or method 'shoes' is referenced which is undefined in the scope in which it is called

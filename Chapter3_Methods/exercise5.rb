bb02684f7c0a1f0ba8542830cbc9b13e2b96ff42
Robin Wagner <robin-wagner@web.de>
2016-12-11 18:58:10 +0000

    Chapter 3 - Exercise 5 completed

---
+++ Chapter3_Methods/exercise5.rb
@@ -0,0 +1,11 @@
+# Exercise 5
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+
+# => Method returns 'nil' (and prints 'Yippeee!!!!' to the screen)
+# puts always returns 'nil' (in contrast to p which returns the actual value)

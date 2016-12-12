6d9be9172977a3688e5eb640029e03cae799a2a2
Robin Wagner <robin-wagner@web.de>
2016-12-12 07:24:09 +0000

    Chapter 4 Exercise 2 completed

---
+++ Chapter4_FlowControl/exercise2.rb
@@ -0,0 +1,10 @@
+# Exercise 2
+
+def all_caps(str)
+  str.length > 10 ? str.upcase : str
+end
+
+puts all_caps('sasd')
+puts all_caps('hello world')
+puts all_caps("Joe Smith")
+puts all_caps("Joe Robertson")

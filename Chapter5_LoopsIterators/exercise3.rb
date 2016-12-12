e08881afc0970fdca123b85a9bfc386d48a7fbd8
Robin Wagner <robin-wagner@web.de>
2016-12-12 22:14:01 +0000

    Chapter 5 - Exercise 3 completed

---
+++ Chapter5_LoopsIterators/exercise3.rb
@@ -0,0 +1,11 @@
+# Exercise 3
+
+top_five_games = ["mario brothers",
+                  "excite bike",
+                  "ring king",
+                  "castlevania",
+                  "double dragon"]
+
+top_five_games.each_with_index do | game, index|
+  puts "#{index + 1}. #{game}"
+end

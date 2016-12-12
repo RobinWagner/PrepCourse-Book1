3a6f335261a87fe2443829ad15a61b819a4bf88e
Robin Wagner <robin-wagner@web.de>
2016-12-12 18:28:57 +0000

    Chapter 4 - Exercise 6 completed

---
+++ Chapter4_FlowControl/exercise6.rb
@@ -0,0 +1,4 @@
+# Exercise 6
+
+# Keyword 'end' is missing: The 'end' keyword ending the method definition is missing (only the end keyword for the if statement is used)
+# This can be fixed by adding another 'end' keyword for the method.

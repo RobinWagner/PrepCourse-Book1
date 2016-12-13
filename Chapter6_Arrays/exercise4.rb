1b7ebce2824dd993977afe2a2329f1672af88a22
Robin Wagner <robin-wagner@web.de>
2016-12-13 17:49:39 +0000

    Chapter 6 - Exercise 4 completed

---
+++ Chapter6_Arrays/exercise4.rb
@@ -0,0 +1,14 @@
+# Exercise 4
+
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+arr.index(5)
+# => 3
+# returns index of first occurence of object within the array
+
+arr.index[5]
+# => NoMethodError: undefined method '[]'
+
+arr[5]
+# => 8
+# returns array element at index 5

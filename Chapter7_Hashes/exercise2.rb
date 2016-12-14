618a87b6a450aa029f867d18055d851672de5fe8
Robin Wagner <robin-wagner@web.de>
2016-12-14 06:27:49 +0000

    Renamed variables

--- Chapter7_Hashes/exercise2.rb
+++ Chapter7_Hashes/exercise2.rb
@@ -1,14 +1,14 @@
 # Exercise 2
 
-arr1 = { Bob: 26, Jane: 43, Lisa: 56 }
-arr2 = { Claire: 31, Malissa: 19 }
+hash1 = { Bob: 26, Jane: 43, Lisa: 56 }
+hash2 = { Claire: 31, Malissa: 19 }
 
 # merge method is not destructive (does not mutate the caller)
 
-arr1.merge(arr2)
-p arr1
+hash1.merge(hash2)
+p hash1
 
 # merge! method is destructive (does mutate the caller)
 
-arr1.merge!(arr2)
-p arr1
+hash1.merge!(hash2)
+p hash1

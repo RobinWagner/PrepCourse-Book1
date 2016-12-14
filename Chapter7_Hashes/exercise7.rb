204df4c3c821fc8e63c188baff37ee5d60878a5a
Robin Wagner <robin-wagner@web.de>
2016-12-14 19:55:59 +0000

    Chapter 7 - Exercise 7 completed

---
+++ Chapter7_Hashes/exercise7.rb
@@ -0,0 +1,6 @@
+# Exercise 7
+
+x = "hi there"
+my_hash = {x: "some value"}       # Created with new hash syntax (introduced with Ruby 1.9) (key is symbol)
+my_hash2 = {x => "some value"}    # Created with original hash syntax (key is string)
+

c2b8462a9373d07ea68674efbe31682823575667
Robin Wagner <robin-wagner@web.de>
2016-12-14 06:10:00 +0000

    Chapter 7 - Exercise 1 completed

---
+++ Chapter7_Hashes/exercise1.rb
@@ -0,0 +1,11 @@
+# Exercise 1
+
+family = { uncles: ["bob", "joe", "steve"],
+           sisters: ["jane", "jill", "beth"],
+           brothers: ["frank", "rob", "david"],
+           aunts: ["mary", "sally", "susan"]
+         }
+
+new_arr = family.select { |k, v| k == :sisters || k == :brothers }.values.flatten
+
+p new_arr

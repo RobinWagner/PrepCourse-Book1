5ad1cc8725b602bc3cb828814a012a4f248536bb
Robin Wagner <robin-wagner@web.de>
2016-12-16 06:33:17 +0000

    Chapter 10 - Exercise 11 completed

---
+++ Chapter10_Exercises/exercise11.rb
@@ -0,0 +1,5 @@
+# Exercise 11
+
+# Favorite Rails / Ruby online API sources:
+# Ruby: ruby-doc (official, very extensive)
+# Rails: api.rubyonrails.org (official, very extensive), guides.rubyonrails.org (great guides)

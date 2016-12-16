2eab53c71b47e22dbeb990fdc95827fa271dedc5
Robin Wagner <robin-wagner@web.de>
2016-12-16 06:23:04 +0000

    Chapter 10 0 Exercise 10 completed

---
+++ Chapter10_Exercises/exercise10.rb
@@ -0,0 +1,7 @@
+# Exercise 10
+
+# Hash values can be arrays
+card_value = { Bob: [2, 3, 4], Charlie: [4, 5, 9] }
+
+# You can have an array of hashes
+array = [ { value1: 2, value2: 4}, { Bob: 34, Claire: 26 }]

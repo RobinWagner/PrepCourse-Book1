be373817bcad953a66b750ef4939bf425fbfea8f
Robin Wagner <robin-wagner@web.de>
2016-12-17 14:17:48 +0000

    Chapter 10 - Exercise 15 completed

---
+++ Chapter10_Exercises/exercise15.rb
@@ -0,0 +1,7 @@
+# Exercise 15
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+arr.delete_if { |element| element.start_with?('s') }
+
+arr.delete_if { |element| element.start_with?('s', 'w') }

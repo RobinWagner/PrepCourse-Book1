14a471154d9eab181c99651458964bb5bb01cdf7
Robin Wagner <robin-wagner@web.de>
2016-12-17 14:46:20 +0000

    Chapter 10 - Exercise 17 completed

---
+++ Chapter10_Exercises/exercise17.rb
@@ -0,0 +1,12 @@
+# Exercise 17
+
+hash1 = {shoes: "nike", "hat" => "adidas", :hoodie => true}
+hash2 = {"hat" => "adidas", :shoes => "nike", hoodie: true}
+
+if hash1 == hash2
+  puts "These hashes are the same!"
+else
+  puts "These hashes are not the same!"
+end
+
+# Will put out "These hashes are the same!" since keys and values are identical (even though not their order).
